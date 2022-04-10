5ad2da1d37233157c6d64a035864dda2ec6cb2b4
Greg <lupasco.grigori@gmail.com>
2022-04-11 01:05:22 +0300

    seat and row seat selection added

--- app/src/main/java/com/example/cinema/Cinema.kt
+++ app/src/main/java/com/example/cinema/Cinema.kt
@@ -1,18 +1,58 @@
 package com.example.cinema
 
 
-const val ROWS = 8
-const val COLS = 7
-
 fun main() {
-    arrangement()
+    println("Enter the number of rows:")
+    val rows = 9//readln().toInt()
+    println("Enter the number of seats in each row:")
+    val seats = 9//readln().toInt()
+    val cinema = MutableList(rows) { MutableList(seats) { 'S' } }
+
+    displayCinema(cinema)
+
+    println()
+    displayCinema(stage(rows, seats, cinema))
+
 }
 
-fun arrangement() {
-    val array = Array(COLS) { CharArray(ROWS) { 'S' } }
+fun displayCinema(list: MutableList<MutableList<Char>>) {
+    val size = list[0].lastIndex + 1
+    println()
     println("Cinema:")
     print(" ")
-    for (i in 1..ROWS) print(" $i")
+    for (i in 1..size) print(" $i")
+    list.forEachIndexed { index, chars -> print("\n${index + 1} ${chars.joinToString(" ")} ") }
     println()
-    array.forEachIndexed { index, chars -> println("${index + 1} ${chars.joinToString(" ")} ") }
+}
+
+fun stage(rows: Int, seats: Int, list: MutableList<MutableList<Char>>): MutableList<MutableList<Char>> {
+    val frontSeatPrice = 10
+    val backSeatPrice = 8
+
+    println("Enter a row number:")
+
+    val rowsNR = readln().toInt() - 1
+
+    println("Enter a seat number in that row:")
+
+    val seatsNR = readln().toInt() - 1
+
+
+    list[rowsNR - 1][seatsNR - 1] = 'B'
+
+    val stage = rows * seats
+//    var price: Int
+//    if (rows % 2 == 0) {
+//        price = (stage / 2) * backSeatPrice + (stage / 2) * frontSeatPrice
+//    } else {
+//        price = (rows / 2 * seats) * frontSeatPrice + (rows - rows / 2) * seats * backSeatPrice
+//    }
+
+    println()
+    println()
+    if (stage < 60) println("Ticket price: $$frontSeatPrice") //${stage * frontSeatPrice}
+    if (stage > 60 && rowsNR > 4) println("Ticket price: $${backSeatPrice}") //${price}
+    if (stage > 60 && rowsNR <= 4) println("Ticket price: $${frontSeatPrice}")
+
+    return list
 }
