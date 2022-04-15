fae69540102b091a1b15dc0acb14352f5207f495
Greg <lupasco.grigori@gmail.com>
2022-04-16 01:37:02 +0300

    menu and statistics selection added

--- app/src/main/java/com/example/cinema/Cinema.kt
+++ app/src/main/java/com/example/cinema/Cinema.kt
@@ -1,58 +1,99 @@
 package com.example.cinema
 
+const val FRONTSEATPRICE = 10
+const val BACKSEATPRICE = 8
 
 fun main() {
     println("Enter the number of rows:")
-    val rows = 9//readln().toInt()
+    val rows = readln().toInt()
     println("Enter the number of seats in each row:")
-    val seats = 9//readln().toInt()
+    val seats = readln().toInt()
     val cinema = MutableList(rows) { MutableList(seats) { 'S' } }
 
-    displayCinema(cinema)
+    var currentIncome = 0
+    var purchasedTickets = 0
 
-    println()
-    displayCinema(stage(rows, seats, cinema))
-
+    while (true) {
+        when (menu()) {
+            1 -> {
+                displayCinema(cinema)
+            }
+            2 -> {
+                val rowsSeat = gettingSeat(cinema)
+                val ticketPrice =
+                    if (cinema.size * cinema[0].size > 60 && rowsSeat[0] < cinema.size / 2 || cinema.size * cinema[0].size < 60) FRONTSEATPRICE else BACKSEATPRICE
+                purchasedTickets++
+                currentIncome += ticketPrice
+                cinema[rowsSeat[0]][rowsSeat[1]] = 'B'
+                println("Ticket price: $$ticketPrice")
+            }
+            3 -> {
+                statistics(currentIncome, purchasedTickets, cinema)
+            }
+            0 -> break
+        }
+    }
 }
 
-fun displayCinema(list: MutableList<MutableList<Char>>) {
-    val size = list[0].lastIndex + 1
+fun menu(): Int {
     println()
-    println("Cinema:")
-    print(" ")
-    for (i in 1..size) print(" $i")
-    list.forEachIndexed { index, chars -> print("\n${index + 1} ${chars.joinToString(" ")} ") }
-    println()
+    println("1. Show the seats")
+    println("2. Buy a ticket")
+    println("3. Statistics")
+    println("0. Exit")
+    return readln().toInt()
 }
 
-fun stage(rows: Int, seats: Int, list: MutableList<MutableList<Char>>): MutableList<MutableList<Char>> {
-    val frontSeatPrice = 10
-    val backSeatPrice = 8
+fun displayCinema(l: MutableList<MutableList<Char>>) {
+    print("\nCinema:\n ")
+    for (i in 1..l[0].size) print(if (i == l[0].size) " $i\n" else " $i")
+    for (i in 1..l.size) println("$i ${l[i - 1].joinToString(" ")}")
+}
 
-    println("Enter a row number:")
+fun gettingSeat(l: MutableList<MutableList<Char>>): MutableList<Int> {
+    var switch = true
 
-    val rowsNR = readln().toInt() - 1
+    var rowsNR = 0
+    var seatNR = 0
 
-    println("Enter a seat number in that row:")
+    do {
+        println("Enter a row number:")
+        val rowsInp = readln().toInt()
+        println("Enter a seat number in that row:")
+        val seatInp = readln().toInt()
 
-    val seatsNR = readln().toInt() - 1
+        if (rowsInp > l.size || seatInp > l[1].size) {
+            println("\nWrong input!\n")
+        } else if (l[rowsInp - 1][seatInp - 1] == 'B') {
+            println("\nThat ticket has already been purchased!\n")
+        } else if (rowsInp <= l.size && seatInp <= l[1].size) {
+            rowsNR = rowsInp - 1
+            seatNR = seatInp - 1
+            switch = false
+        }
+    } while (switch)
+    return mutableListOf(rowsNR, seatNR)
+}
 
-
-    list[rowsNR - 1][seatsNR - 1] = 'B'
-
-    val stage = rows * seats
-//    var price: Int
-//    if (rows % 2 == 0) {
-//        price = (stage / 2) * backSeatPrice + (stage / 2) * frontSeatPrice
-//    } else {
-//        price = (rows / 2 * seats) * frontSeatPrice + (rows - rows / 2) * seats * backSeatPrice
-//    }
-
+fun statistics(currentIncome: Int, purchasedTickets: Int, l: MutableList<MutableList<Char>>) {
     println()
-    println()
-    if (stage < 60) println("Ticket price: $$frontSeatPrice") //${stage * frontSeatPrice}
-    if (stage > 60 && rowsNR > 4) println("Ticket price: $${backSeatPrice}") //${price}
-    if (stage > 60 && rowsNR <= 4) println("Ticket price: $${frontSeatPrice}")
 
-    return list
+    val hallSize = l.size * l[0].size
+    val totalIncome = if (l.size * l[0].size <= 60) {
+        l.size * l[0].size * FRONTSEATPRICE
+    } else {
+        ((l.size / 2 * l[0].size) * FRONTSEATPRICE) + (l.size - l.size / 2) * l[0].size * BACKSEATPRICE
+    }
+
+    val percentage = purchasedTickets.toDouble() / hallSize.toDouble() * 100.0
+    val formatPercentage = "%.2f".format(percentage)
+
+    val stat = """
+        Number of purchased tickets: $purchasedTickets
+        Percentage: $formatPercentage%
+        Current income: $${currentIncome}
+        Total income: $${totalIncome}
+        """.trimIndent()
+    print(stat)
+    println()
 }
